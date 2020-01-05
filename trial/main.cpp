019619231bc77ba71761eb710d155cb2896abf7c
anas1993 <shaikhanas1993@gmail.com>
2020-01-05 16:00:05 +0530

    added all files

---
+++ trial/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    auto foo = "bar";
+    cout << foo  << endl;
+    cout << "hello world!!" << endl;
+    cout << "this is test" << endl;
+}
