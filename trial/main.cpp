de002242a40a8ffa16aca387a6a7b4a72ffd7781
anas1993 <shaikhanas1993@gmail.com>
2020-01-05 19:33:58 +0530

    files containing dummy code

--- trial/main.cpp
+++ trial/main.cpp
@@ -1,9 +1,18 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
+#define PI 3.14
+#define NEWLINE '\n'
+#define newFLoat "stinggggg"
+
+auto const test_variable = 2.21;
+
 int main(){
-    auto foo = "bar";
-    cout << foo  << endl;
-    cout << "hello world!!" << endl;
-    cout << "this is test" << endl;
+   int a{10};
+   int b(5);
+   a += b;
+   cout << "the value of  a ::" << " " << a << " " << endl;
+   cout << "the value of b ::" << " " << b << endl;
+   
 }
